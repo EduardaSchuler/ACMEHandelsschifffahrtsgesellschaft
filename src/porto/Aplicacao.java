be36c41597cd6c030ebfe64977bda09360fb7116
Maria Eduarda Schüler <45366066+EduardaSchuler@users.noreply.github.com>
2023-06-12 01:10:16 -0300

    Cria construtor app para exibir painel - esta com erro ao adicionar o porto na lista

--- src/porto/Aplicacao.java
+++ src/porto/Aplicacao.java
@@ -1,4 +1,18 @@
 package porto;
 
-public class Aplicacao {
+import javax.swing.*;
+
+public class Aplicacao extends JFrame {
+    private FormPortoGUI formPortoGUI;
+
+    public Aplicacao(){
+        super();
+        formPortoGUI = new FormPortoGUI(this);
+        this.setContentPane(formPortoGUI.getPainel());
+        this.setTitle("Cadastro de Portos");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.pack();
+        this.setSize(800,200);
+        this.setVisible(true);
+    }
 }
